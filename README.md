b2a7e50d85d97e238c29ccc50479ea2fb9780fda
Augusto César Melo <augustocesarmelo@gmail.com>
2019-07-16 00:07:30 -0300

    Adicionando o express

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Acompanhamento do Curso de Graphql
 * npm i -D -E typescript@2.5.3
 * npm i -D -E gulp@3.9.1 gulp-typescript@3.2.2 gulp-clean@0.3.2
 * npm i -D -E nodemon@1.12.0
+* npm i -S -E express@4.16.2
+* npm i -D -E @types/express@4.0.37 @types/node@8.0.34
 
 ### build do projeto
 * npm run gulp
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-console.log("Olá Mundo");
+import * as express from 'express';
 
-console.log("alteração lida pelo nodemon")
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+        this.express.use('/hello', (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+            resp.send({
+                hello: 'Hello World!'
+            });
+        });
+    }
+}
+
+export default new App().express;
