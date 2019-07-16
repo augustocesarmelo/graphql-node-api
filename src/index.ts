426d716cb74790fd524f4354f02aea2f1b4247cc
Augusto César Melo <augustocesarmelo@gmail.com>
2019-07-16 16:41:44 -0300

    onListening, onError e normalizePort

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import * as http from 'http';
 import app from './app';
+import {normalizePort, onError, onListening} from './utils/utils';
 
 const server = http.createServer(app);
+const port = normalizePort(process.env.PORT || 3000);
+//const { port } = server.address() as AddressInfo;
 
-server.listen(3000);
-server.on('listening', () => console.log('listening on port 3000 ...'));
+server.listen(port);
+server.on('error', onError(server));
+server.on('listening', onListening(server));
